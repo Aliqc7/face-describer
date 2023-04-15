f1a2fe0abcccd8471a8ee5ba155c5241c9c73a79
Ali <ali.zolghadr@gmail.com>
2023-04-15 18:53:13 +1000

    streamlit app file, first commit

---
+++ face-app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+page_title = "Face describer"
+page_icon = ":male-detective:"
+layout = "centered"
+
+
+st.set_page_config(page_title=page_title, page_icon=page_icon, layout=layout)
+st.title(page_title + "" + page_icon)
+st.write("Face describer app coming up soon!")

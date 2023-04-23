b183651978f02053b46bea12d6d593d6840ba9f3
Ali <ali.zolghadr@gmail.com>
2023-04-23 16:50:43 +1000

    Fix Openai response's indexing mismatch

--- face_app.py
+++ face_app.py
@@ -1,10 +1,13 @@
 import streamlit as st
 import numpy as np
-import cv2
 import face_util
 import tensorflow as tf
 from mtcnn import MTCNN
 import openai
+import json
+
+secret_name = "Openai_api_key"
+region_name = "us-east-1"
 
 page_title = "Face describer"
 page_icon = ":male-detective:"
@@ -12,8 +15,6 @@ layout = "centered"
 
 image_size = (224, 224)
 image_name = "image.jpg"
-# TODO remove this key
-openai_api_key = "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"
 
 file_types = ["bmp", "dib", "jpeg", "jpg", "jpe", "jp2",
               "png", "webp", "pbm", "pgm", "ppm", "pxm", "pnm",
@@ -28,9 +29,6 @@ st.title(page_title + "" + page_icon)
 uploaded_file = st.file_uploader("Upload an image of a face!", type=file_types)
 
 
-# CV2
-
-
 @st.cache_resource
 def load_model(model_name):
     tr_model = tf.keras.models.load_model(model_name)
@@ -39,18 +37,17 @@ def load_model(model_name):
 
 @st.cache_resource
 def create_mtcnn_detector():
-    detector = MTCNN()
-    return detector
+    return MTCNN()
 
 
 @st.cache_resource
 def set_open_ai_key():
-    openai.api_key = openai_api_key
+    return face_util.get_openai_api_key(secret_name, region_name)
 
 
-trained_model = load_model("face_eff_net_all_discarded_bad_samples")
+trained_model = load_model("face_eff_net_trained_on_all_cleaned_data")
 detector = create_mtcnn_detector()
-set_open_ai_key()
+openai.api_key = set_open_ai_key()
 
 if uploaded_file is not None:
     img_str = uploaded_file.getvalue()
@@ -63,34 +60,48 @@ if uploaded_file is not None:
         st.write("There is 1 person in this image")
     else:
         st.write("There is no one in this image")
+
+    face_descriptions_dict = dict()
+
+    for i in range(n_face):
+        image = face_util.read_and_prepare_image_as_input(image_size, f"detected_face{i}.jpg")
+        prediction_scores = trained_model.predict(np.expand_dims(image.numpy(), axis=0))
+        predicted_attributes = []
+        for j, label in enumerate(attributes):
+            if prediction_scores[j][0][0] > 0.5:
+                predicted_attributes.append(f"{label}")
+
+        if "Male" not in predicted_attributes:
+            predicted_attributes.append("Female")
+        if "No_Beard" in predicted_attributes:
+            predicted_attributes.remove("No_Beard")
+        elif all(elem not in predicted_attributes for elem in ["Goatee", "5_o_Clock_Shadow", "Mustache"]):
+            predicted_attributes.append("Beard")
+
+        face_descriptions_dict[f"person{i + 1}"] = predicted_attributes
+
+    content = f"These are the characteristics of {n_face} people: {face_descriptions_dict}. " \
+              f"Describe them using full sentences. " \
+              f"If there are more than one person, refer to them by their ordinal number e.g (The first person), " \
+              f"otherwise refer to the person as 'the person'. Always start with their genders. " \
+              f"Stick to the provided characteristics do not make conclusions. Keep it as simple as possible. " \
+              f"Return the response in JSON format. The keys of this dictionary must be person numbers (e.g., 1)" \
+              f"The values of the dictionary must be the corresponding person's description. " \
+              f"values." \
+              f"Put keys and values in double quotes."
+
+    indexes = set([str(i) for i in range(1, n_face + 1)])
+    try_describing = True
+    response_dict = {}
+    while try_describing:
+        completion = openai.ChatCompletion.create(model="gpt-3.5-turbo",
+                                                  messages=[{"role": "user", "content": content}])
+        response_dict = json.loads(completion.choices[0].message.content)
+        if set(response_dict.keys()) == indexes:
+            try_describing = False
+
     for i in range(n_face):
         with st.container():
             image = face_util.read_and_prepare_image_as_input(image_size, f"detected_face{i}.jpg")
             st.image(image.numpy())
-            prediction_scores = trained_model.predict(np.expand_dims(image.numpy(), axis=0))
-            predicted_attributes = []
-            for j, label in enumerate(attributes):
-                if prediction_scores[j][0][0] > 0.5:
-                    predicted_attributes.append(f"{label}")
-
-
-            person_number = i + 1
-            content = f"These are the characteristics of person number {person_number}: {predicted_attributes}. " \
-                      f"Describe them. " \
-                      f"Refer to the person by their ordinal number e.g., The first person. Start with their gender. " \
-                      f"Stick to the provided " \
-                      f"characteristics do not make conclusions. Keep it as simple as possible"
-            completion = openai.ChatCompletion.create(model="gpt-3.5-turbo",
-                                                      messages=[{"role": "user", "content": content}])
-            st.write(completion.choices[0].message.content)
-
-    # np_arr = np.fromstring(img_str, np.uint8)
-    # image_tf = tf.constant(np_arr)
-    # image = tf.image.resize(image_tf, image_size)
-    # normalization_layer = tf.keras.layers.Rescaling(1. / 255)
-    # image = normalization_layer(image)
-    #
-    #
-    # img_np = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-    # image = face_util.prepare_image(img_np, image_size)
-    # st.image(img_np)
+            st.write(response_dict[f"{i + 1}"])
